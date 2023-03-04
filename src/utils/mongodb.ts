77d6b6dfb64bd4f1ae3d58d5a487e12d6b61993a
K93MSY <masaya93@ogaki-tv.ne.jp>
2023-03-04 23:15:42 +0900

    fix

--- src/utils/mongodb.ts
+++ src/utils/mongodb.ts
@@ -1,8 +1,3 @@
-import MongoClient from 'mongodb';
-const uri = process.env.​MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+import {MongoClient} from 'mongodb';
+const uri:string = process.env.​MONGODB_URI!;
+const client = new MongoClient(uri);
